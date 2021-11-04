ebbefbccbf54ce083d4a1cab24f5b0e64533f498
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-11-04 22:10:40 +0300

    feat: modified navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,11 +5,11 @@ import Search from './Search';
 
 export default function NavBar(props) {
     return (
-      <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
+      <div className="p-5 pb-2 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
         <div className="flex justify-between items-center space-x-5 w-screen ">
           <Link to="/">
             <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
-              Google 🔎
+              Googla 🔎
             </p>
           </Link>
           <button type="button" onClick={() => props.setDarkTheme(!props.darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">{!props.darkTheme ? '💡 Light' : '🌙 Dark'}</button>
