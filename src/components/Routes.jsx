9ef691aa8811523b95eff8b8b43096e11d31e585
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-11-04 22:11:02 +0300

    feat: modified routes component

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router';
+import Results from './Results';
 
 export default function Routes() {
     return (
         <div>
-            
+            <Switch>
+              <Route exact path='/'>
+                <Redirect to='/search' />
+              </Route>
+              <Route exact path='/search'>
+                <Results />
+              </Route>
+              <Route path='/images'>
+                <Results />
+              </Route>
+              <Route path='/news'>
+                <Results />
+              </Route>
+              <Route path='/videos'>
+                <Results />
+              </Route>
+            </Switch>
         </div>
     )
 }
