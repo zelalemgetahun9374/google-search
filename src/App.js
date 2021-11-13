d5f560066464705ae9eb48b8073f01ee3bec043e
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-11-13 16:58:21 +0300

    feat: updated app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import NavBar from "./components/NavBar";
+import Routes from "./components/Routes";
+import Footer from "./components/Footer";
+
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div>
-        App
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+        <NavBar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
+        <Routes />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import StateContextProvider from './contexts/StateContextProvider';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StateContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StateContextProvider>,
   document.getElementById('root')
 );
-
